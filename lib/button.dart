a93cf172c949f534c34b863b11ef42aa85b63e23
MarwanGhareeb <marwanghareeb146@gmail.com>
2025-07-28 22:14:01 +0300

    modify button file to allow delete or insert when long press

--- lib/button.dart
+++ lib/button.dart
@@ -2,7 +2,7 @@ import 'package:calculator/common_colors.dart';
 import 'package:calculator/equation_provider.dart';
 import 'package:flutter/material.dart';
 
-class Button extends StatelessWidget {
+class Button extends StatefulWidget {
   final String char;
   final VoidCallback? onPress;
   final Icon? icon;
@@ -10,45 +10,77 @@ class Button extends StatelessWidget {
   const Button({super.key, required this.char, this.onPress, this.icon});
 
   @override
+  State<Button> createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  bool _isDeleting = false;
+  bool _isInserting = false;
+
+  @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ButtonStyle(
-        backgroundColor: WidgetStatePropertyAll(
-          double.tryParse(char) != null
-              ? BLUE
-              : char == 'C'
-              ? Colors.red
-              : (icon != null || char == '=' || char == '.')
-              ? BLACK
-              : GREEN,
-        ),
-      ),
-      onPressed:
-          onPress ??
-          () => icon == null
-              ? insertEquation(context, char)
-              : deleteAtCursor(context),
-      onLongPress: () =>
-          icon != null ? getEquationController(context).text = '' : null,
-      child:
-          icon ??
-          Text(
-            char,
-            style: TextStyle(
-              fontSize: 30,
-              color: (char == '=' || char == '.') ? GREEN : WHITE,
-              fontWeight: FontWeight.w900,
-            ),
+    final equation = getEquationController(context);
+
+    return GestureDetector(
+      onLongPressStart: widget.icon != null
+          ? (_) {
+              _isDeleting = true;
+              _startDeleting(equation);
+              setState(() {});
+            }
+          : (_) {
+              _isInserting = true;
+              _startInserting(equation);
+              setState(() {});
+            },
+      onLongPressEnd: widget.icon != null
+          ? (_) {
+              _isDeleting = false;
+              setState(() {});
+            }
+          : (_) {
+              _isInserting = false;
+              setState(() {});
+            },
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll(
+            double.tryParse(widget.char) != null
+                ? BLUE
+                : widget.char == 'C'
+                ? Colors.red
+                : (widget.icon != null ||
+                      widget.char == '=' ||
+                      widget.char == '.')
+                ? BLACK
+                : GREEN,
           ),
+        ),
+        onPressed:
+            widget.onPress ??
+            () => widget.icon == null
+                ? insertEquation(equation, widget.char)
+                : deleteAtCursor(equation),
+        child:
+            widget.icon ??
+            Text(
+              widget.char,
+              style: TextStyle(
+                fontSize: 30,
+                color: (widget.char == '=' || widget.char == '.')
+                    ? GREEN
+                    : WHITE,
+                fontWeight: FontWeight.w900,
+              ),
+            ),
+      ),
     );
   }
 
   TextEditingController getEquationController(BuildContext context) =>
       EquationProvider.of(context)?.equation ?? TextEditingController();
 
-  void insertEquation(BuildContext context, String char) {
-    final TextEditingController equation = getEquationController(context);
-
+  void insertEquation(TextEditingController equation, String char) {
     final selection = equation.selection;
     final int start = selection.start;
     final int end = selection.end;
@@ -64,9 +96,7 @@ class Button extends StatelessWidget {
     equation.selection = TextSelection.collapsed(offset: start + char.length);
   }
 
-  void deleteAtCursor(BuildContext context) {
-    final TextEditingController equation = getEquationController(context);
-
+  void deleteAtCursor(TextEditingController equation) {
     final text = equation.text;
     final selection = equation.selection;
     final int start = selection.start;
@@ -82,6 +112,22 @@ class Button extends StatelessWidget {
       final newText = text.replaceRange(start - 1, end, '');
       equation.text = newText;
       equation.selection = TextSelection.collapsed(offset: start - 1);
+    }
+  }
+
+  Future<void> _startDeleting(TextEditingController equation) async {
+    while (_isDeleting && equation.text.isNotEmpty) {
+      await Future.delayed(Duration(milliseconds: 80));
+      if (!_isDeleting) break;
+      deleteAtCursor(equation);
+    }
+  }
+
+  Future<void> _startInserting(TextEditingController equation) async {
+    while (_isInserting) {
+      await Future.delayed(Duration(milliseconds: 80));
+      if (!_isInserting) break;
+      insertEquation(equation, widget.char);
     }
   }
 }
